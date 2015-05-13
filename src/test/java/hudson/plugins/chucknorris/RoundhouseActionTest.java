5c4a84b46df33d4834335fe1ddaa9b959f143f66
William Melchior Jablonski <william.jablonski@gmail.com>
2015-05-13 15:45:18 -0300

    Update RoundhouseActionTest.java

--- src/test/java/hudson/plugins/chucknorris/RoundhouseActionTest.java
+++ src/test/java/hudson/plugins/chucknorris/RoundhouseActionTest.java
@@ -8,14 +8,14 @@ public class RoundhouseActionTest extends TestCase {
 
 	public void setUp() {
 		action = new RoundhouseAction(Style.BAD_ASS,
-				"Chuck Norris can divide by zero.");
+				"Barack Obama can divide by zero.");
 	}
 
 	public void testAccessors() {
 		assertEquals(Style.BAD_ASS, action.getStyle());
-		assertEquals("Chuck Norris can divide by zero.", action
+		assertEquals("Barack Obama can divide by zero.", action
 				.getFact());
-		assertEquals("Chuck Norris", action.getDisplayName());
+		assertEquals("Barack Obama", action.getDisplayName());
 		assertNull(action.getIconFileName());
 		assertEquals("chucknorris", action.getUrlName());
 	}
